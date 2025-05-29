9752abe11a665cefb59f70b76934ff0e1ca4fad2
masha256 <mike.machado@smartcontract.com>
2025-05-28 18:25:02 -0700

    cleanup signature handling

--- events/verifier/verifier.go
+++ events/verifier/verifier.go
@@ -1,6 +1,8 @@
 package verifier
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"os"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -33,10 +35,12 @@ func NewEventVerifier(opts *EventVerifierOptions) *EventVerifier {
 	}
 }
 
-func (v *EventVerifier) Verify(event *types.VerifiableEvent) (bool, error) {
-	v.Log.Debug().Str("eventType", event.Type).Msg("Verifying event")
+func (v *EventVerifier) Verify(verifiableEvent *types.VerifiableEvent) (bool, error) {
+	v.Log.Debug().Str("eventType", verifiableEvent.Type).Msg("Verifying verifiableEvent")
 
-	hash := crypto.Keccak256Hash([]byte(event.Payload))
+	hash := crypto.Keccak256Hash([]byte(verifiableEvent.Type + verifiableEvent.Payload))
+
+	v.Log.Debug().Str("hash", hash.Hex()).Msg("Verifying event hash")
 
 	validSigCount := 0
 	availableSigners := make(map[common.Address]bool)
@@ -45,22 +49,46 @@ func (v *EventVerifier) Verify(event *types.VerifiableEvent) (bool, error) {
 		availableSigners[common.HexToAddress(signer)] = true
 	}
 
-	for _, sig := range event.Signatures {
-		pubKey, err := crypto.SigToPub(hash.Bytes(), common.Hex2Bytes(sig))
+	for _, sig := range verifiableEvent.Signatures {
+		sigBytes, err := common.ParseHexOrString(sig)
+		if err != nil {
+			v.Log.Error().Err(err).Msg("Failed to parse signature")
+			return false, err
+		}
+		if sigBytes[64] == 27 || sigBytes[64] == 28 {
+			sigBytes[64] -= 27 // Adjust signature format for Ethereum signatures
+		}
+		pubKey, err := crypto.SigToPub(hash.Bytes(), sigBytes)
 		if err != nil {
 			v.Log.Error().Err(err).Msg("Failed to recover public key from signature")
 			return false, err
 		}
 
 		signer := crypto.PubkeyToAddress(*pubKey)
+		v.Log.Debug().Str("signer", signer.Hex()).Msg("Recovered signer from signature")
 
 		if availableSigners[signer] {
-			v.Log.Debug().Str("signer", signer.Hex()).Msg("Signature verified successfully")
+			v.Log.Info().Str("signer", signer.Hex()).Msg("Signature verified successfully")
 			validSigCount++
 			availableSigners[signer] = false // Mark this signer as used
+		}
+
+		// If we have enough valid signatures, we can return early
+		if validSigCount >= v.MinRequiredSignatures {
+			return true, nil
 		}
 	}
 	v.Log.Debug().Int("validSigCount", validSigCount).Msg("Total valid signatures")
 
 	return validSigCount >= v.MinRequiredSignatures, nil
+}
+
+func (v *EventVerifier) Decode(verifiableEvent *types.VerifiableEvent, event any) error {
+	decodedStr, err := base64.StdEncoding.DecodeString(verifiableEvent.Payload)
+	if err != nil {
+		v.Log.Error().Err(err).Msg("Failed to decode base64 payload")
+		return err
+	}
+
+	return json.Unmarshal(decodedStr, event)
 }
