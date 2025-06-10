81f517e60b3b2f9fa75e9e4534d0e16cf4090e58
masha256 <mike.machado@smartcontract.com>
2025-06-10 07:34:57 -0700

    add token approval mechanism to ccip service

--- Makefile
+++ Makefile
@@ -8,3 +8,4 @@ generate:
 	go-jsonschema services/dvp/schema/dvp.json -p events -o services/dvp/gen/events/events.go -t
 	abigen --abi services/dvp/abi/CCIPDVPCoordinator.abi.json --pkg contract --out services/dvp/gen/contract/contract.go
 	abigen --abi services/ccip/abi/IRouter.abi.json --pkg router --out services/ccip/gen/router/router.go
+	abigen --abi services/ccip/abi/IERC20.abi.json --pkg erc20 --out services/ccip/gen/erc20/erc20.go
--- services/ccip/ccip.go
+++ services/ccip/ccip.go
@@ -8,15 +8,17 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/rs/zerolog"
 
+	"github.com/smartcontractkit/cvn-sdk/services/ccip/gen/erc20"
 	transactTypes "github.com/smartcontractkit/cvn-sdk/transactor/types"
 
 	"github.com/smartcontractkit/cvn-sdk/services/ccip/gen/router"
 )
 
 type CcipServiceOptions struct {
-	Logger            *zerolog.Logger
-	CcipRouterAddress string
-	AccountAddress    string
+	Logger                *zerolog.Logger
+	CcipRouterAddress     string
+	AccountAddress        string
+	IncludeTokenApprovals bool
 }
 
 type CcipService struct {
@@ -42,13 +44,20 @@ func NewCcipService(opts *CcipServiceOptions) *CcipService {
 func (s *CcipService) PrepareRouteMessageOperation(
 	message router.ClientAny2EVMMessage, gasForCallExactCheck uint16, gasLimit *big.Int, receiver common.Address,
 ) (*transactTypes.Operation, error) {
-	abi, err := router.RouterMetaData.GetAbi()
+
+	erc20Abi, err := erc20.Erc20MetaData.GetAbi()
+	if err != nil {
+		s.logger.Error().Err(err).Msg("failed to get ERC20 ABI")
+		return nil, err
+	}
+
+	routerAbi, err := router.RouterMetaData.GetAbi()
 	if err != nil {
 		s.logger.Error().Err(err).Msg("failed to get CCIP router ABI")
 		return nil, err
 	}
 
-	routeMessageCalldata, err := abi.Pack("routeMessage", message, gasForCallExactCheck, gasLimit, receiver)
+	routeMessageCalldata, err := routerAbi.Pack("routeMessage", message, gasForCallExactCheck, gasLimit, receiver)
 	if err != nil {
 		s.logger.Error().Err(err).Msg("failed to pack calldata for routeMessage")
 		return nil, err
@@ -57,15 +66,38 @@ func (s *CcipService) PrepareRouteMessageOperation(
 	ccipRouter := common.HexToAddress(s.options.CcipRouterAddress)
 	account := common.HexToAddress(s.options.AccountAddress)
 
-	return &transactTypes.Operation{
-		ID:      big.NewInt(time.Now().Unix()),
-		Account: &account,
-		Transactions: []*transactTypes.Transaction{
-			{
-				To:    &ccipRouter,
-				Value: big.NewInt(0),
-				Data:  routeMessageCalldata,
-			},
+	var transactions []*transactTypes.Transaction
+
+	if s.options.IncludeTokenApprovals && len(message.DestTokenAmounts) > 0 {
+		// If the message contains token amounts, we need to approve tokens to the CCIP router
+		for _, destTokenAmount := range message.DestTokenAmounts {
+			approveCalldata, err := erc20Abi.Pack("approve", ccipRouter, destTokenAmount.Amount)
+			if err != nil {
+				s.logger.Error().Err(err).Msg("failed to pack calldata for token approve")
+				return nil, err
+			}
+
+			transactions = append(
+				transactions, &transactTypes.Transaction{
+					To:    &destTokenAmount.Token,
+					Value: big.NewInt(0),
+					Data:  approveCalldata,
+				},
+			)
+		}
+	}
+
+	transactions = append(
+		transactions, &transactTypes.Transaction{
+			To:    &ccipRouter,
+			Value: big.NewInt(0),
+			Data:  routeMessageCalldata,
 		},
+	)
+
+	return &transactTypes.Operation{
+		ID:           big.NewInt(time.Now().Unix()),
+		Account:      &account,
+		Transactions: transactions,
 	}, nil
 }
