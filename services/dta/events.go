1ad0dd70d55559e30d253fb69d5dc4ca3a5844df
Malec <cmalec@users.noreply.github.com>
2025-08-22 17:05:53 -0700

    add AnswerUpdated

--- services/dta/events.go
+++ services/dta/events.go
@@ -6,7 +6,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-// All events from DTAOpenMarketplaceU.abi.json
+// All events from DTAOpenMarketplaceU.abi.json expect AnswerUpdated
 
 type EventName string
 
@@ -25,6 +25,7 @@ const (
 	EventOwnershipTransferred         EventName = "OwnershipTransferred"
 	EventRedemptionRequested          EventName = "RedemptionRequested"
 	EventSubscriptionRequested        EventName = "SubscriptionRequested"
+	EventAnswerUpdated                EventName = "AnswerUpdated"
 )
 
 var allEvents = map[string]EventName{
@@ -42,6 +43,7 @@ var allEvents = map[string]EventName{
 	string(EventOwnershipTransferred):         EventOwnershipTransferred,
 	string(EventRedemptionRequested):          EventRedemptionRequested,
 	string(EventSubscriptionRequested):        EventSubscriptionRequested,
+	string(EventAnswerUpdated):                EventAnswerUpdated,
 }
 
 func parseEvent(value string) (EventName, bool) {
@@ -55,101 +57,108 @@ func (ev EventName) String() string {
 
 // DistributorRegistered (address distributorAddr)
 type DistributorRegistered struct {
-	DistributorAddr common.Address
+	DistributorAddr common.Address `json:"distributor_addr"`
 }
 
 // DistributorRequestCanceled (bytes32 fundTokenId, address distributorAddr, bytes32 requestId)
 type DistributorRequestCanceled struct {
-	FundTokenId     [32]byte
-	DistributorAddr common.Address
-	RequestId       [32]byte
+	FundTokenId     [32]byte       `json:"fund_token_id"`
+	DistributorAddr common.Address `json:"distributor_addr"`
+	RequestId       [32]byte       `json:"request_id"`
 }
 
 // DistributorRequestProcessed (bytes32 requestId, uint256 shares, uint8 status, bytes error)
 type DistributorRequestProcessed struct {
-	RequestId [32]byte
-	Shares    *big.Int
-	Status    uint8
-	Error     []byte
+	RequestId [32]byte `json:"request_id"`
+	Shares    *big.Int `json:"shares"`
+	Status    uint8    `json:"status"`
+	Error     []byte   `json:"error"`
 }
 
 // DistributorRequestProcessing (bytes32 fundTokenId, address distributorAddr, bytes32 requestId, uint256 shares, uint256 amount)
 type DistributorRequestProcessing struct {
-	FundTokenId     [32]byte
-	DistributorAddr common.Address
-	RequestId       [32]byte
-	Shares          *big.Int
-	Amount          *big.Int
+	FundTokenId     [32]byte       `json:"fund_token_id"`
+	DistributorAddr common.Address `json:"distributor_addr"`
+	RequestId       [32]byte       `json:"request_id"`
+	Shares          *big.Int       `json:"shares"`
+	Amount          *big.Int       `json:"amount"`
 }
 
 // FundAdminRegistered (address fundAdminAddr)
 type FundAdminRegistered struct {
-	FundAdminAddr common.Address
+	FundAdminAddr common.Address `json:"fund_admin_addr"`
 }
 
 // FundTokenAllowlistUpdated (address fundAdminAddr, bytes32 fundTokenId, address distributorAddr, bool allowed)
 type FundTokenAllowlistUpdated struct {
-	FundAdminAddr   common.Address
-	FundTokenId     [32]byte
-	DistributorAddr common.Address
-	Allowed         bool
+	FundAdminAddr   common.Address `json:"fund_admin_addr"`
+	FundTokenId     [32]byte       `json:"fund_token_id"`
+	DistributorAddr common.Address `json:"distributor_addr"`
+	Allowed         bool           `json:"allowed"`
 }
 
 // FundTokenRegistered (address fundAdminAddr, bytes32 fundTokenId, address fundTokenAddr, address navAddr, uint64 tokenChainSelector)
 type FundTokenRegistered struct {
-	FundAdminAddr      common.Address
-	FundTokenId        [32]byte
-	FundTokenAddr      common.Address
-	NavAddr            common.Address
-	TokenChainSelector uint64
+	FundAdminAddr      common.Address `json:"fund_admin_addr"`
+	FundTokenId        [32]byte       `json:"fund_token_id"`
+	FundTokenAddr      common.Address `json:"fund_token_addr"`
+	NavAddr            common.Address `json:"nav_addr"`
+	TokenChainSelector uint64         `json:"token_chain_selector"`
 }
 
 // Initialized (uint64 version)
 type Initialized struct {
-	Version uint64
+	Version uint64 `json:"version"`
 }
 
 // InvalidDTAWallet (address fundAdminAddr, bytes32 fundTokenId, bytes32 requestId, uint64 actualChainSelector, address actualDTAAdminWalletAddr)
 type InvalidDTAWallet struct {
-	FundAdminAddr            common.Address
-	FundTokenId              [32]byte
-	RequestId                [32]byte
-	ActualChainSelector      uint64
-	ActualDTAAdminWalletAddr common.Address
+	FundAdminAddr            common.Address `json:"fund_admin_addr"`
+	FundTokenId              [32]byte       `json:"fund_token_id"`
+	RequestId                [32]byte       `json:"request_id"`
+	ActualChainSelector      uint64         `json:"actual_chain_selector"`
+	ActualDTAAdminWalletAddr common.Address `json:"actual_dta_admin_wallet_addr"`
 }
 
 // MessageFailed (bytes32 messageId, bytes reason)
 type MessageFailed struct {
-	MessageId [32]byte
-	Reason    []byte
+	MessageId [32]byte `json:"message_id"`
+	Reason    []byte   `json:"reason"`
 }
 
 // NativeFundsRecovered (address to, uint256 amount)
 type NativeFundsRecovered struct {
-	To     common.Address
-	Amount *big.Int
+	To     common.Address `json:"to"`
+	Amount *big.Int       `json:"amount"`
 }
 
 // OwnershipTransferred (address previousOwner, address newOwner)
 type OwnershipTransferred struct {
-	PreviousOwner common.Address
-	NewOwner      common.Address
+	PreviousOwner common.Address `json:"previous_owner"`
+	NewOwner      common.Address `json:"new_owner"`
 }
 
 // RedemptionRequested (bytes32 fundTokenId, address distributorAddr, bytes32 requestId, uint256 shares, uint40 createdAt)
 type RedemptionRequested struct {
-	FundTokenId     [32]byte
-	DistributorAddr common.Address
-	RequestId       [32]byte
-	Shares          *big.Int
-	CreatedAt       uint64 // uint40 in Solidity -> uint64 in Go
+	FundTokenId     [32]byte       `json:"fund_token_id"`
+	DistributorAddr common.Address `json:"distributor_addr"`
+	RequestId       [32]byte       `json:"request_id"`
+	Shares          *big.Int       `json:"shares"`
+	CreatedAt       uint64         `json:"created_at"` // uint40 in Solidity -> uint64 in Go
 }
 
 // SubscriptionRequested (bytes32 fundTokenId, address distributorAddr, bytes32 requestId, uint256 amount, uint40 createdAt)
 type SubscriptionRequested struct {
-	FundTokenId     [32]byte
-	DistributorAddr common.Address
-	RequestId       [32]byte
-	Amount          *big.Int
-	CreatedAt       uint64 // uint40 in Solidity -> uint64 in Go
+	FundTokenId     [32]byte       `json:"fund_token_id"`
+	DistributorAddr common.Address `json:"distributor_addr"`
+	RequestId       [32]byte       `json:"request_id"`
+	Amount          *big.Int       `json:"amount"`
+	CreatedAt       uint64         `json:"created_at"` // uint40 in Solidity -> uint64 in Go
+}
+
+// AnswerUpdated (int256 indexed current, uint256 indexed roundId, uint256 updatedAt) note: not fromIDecimalAggregator.sol
+type AnswerUpdated struct {
+	Current   *big.Int `json:"current"`
+	RoundId   *big.Int `json:"roundId"`
+	UpdatedAt *big.Int `json:"updatedAt"`
 }
