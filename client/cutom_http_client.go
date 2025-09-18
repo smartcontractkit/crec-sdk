36575141b46130cfe7992f0f1d8b133d70301ba8
mounzer-masri <monzer.masri@smartcontract.com>
2025-09-18 16:35:30 +0300

    clean code

--- client/cutom_http_client.go
+++ client/cutom_http_client.go
@@ -24,47 +24,7 @@ func (l *CustomHttpClient) Do(req *http.Request) (*http.Response, error) {
 
 		curl, _ := http2curl.GetCurlCommand(req)
 		log.Println(curl.String())
-		/*if curlCmd, err := DumpRequestAsCurl(req); err == nil {
-			l.Logger.Debug().Msg(curlCmd)
-		}*/
 	}
 
 	return l.Client.Do(req)
 }
-
-/*
-func DumpRequestAsCurl(req *http.Request) (string, error) {
-	var curlCmd strings.Builder
-
-	// Start with method + URL
-	fmt.Fprintf(&curlCmd, "curl -X %s '%s'", req.Method, req.URL.String())
-
-	// Headers
-	for name, values := range req.Header {
-		for _, v := range values {
-			fmt.Fprintf(&curlCmd, " -H '%s: %s'", name, v)
-		}
-	}
-
-	// Body (if any, and rewindable)
-	if req.Body != nil {
-		// Copy body so we don’t consume the original
-		var buf bytes.Buffer
-		bodyCopy := io.TeeReader(req.Body, &buf)
-
-		b, err := io.ReadAll(bodyCopy)
-		if err != nil {
-			return "", err
-		}
-
-		// restore body for actual request sending
-		req.Body = io.NopCloser(&buf)
-
-		if len(b) > 0 {
-			fmt.Fprintf(&curlCmd, " --data '%s'", string(b))
-		}
-	}
-
-	return curlCmd.String(), nil
-}
-*/
