950fb7f1b57566f98fdbc356f9d978973c143d31
masha256 <mike.machado@smartcontract.com>
2025-09-22 09:20:36 -0700

    remove logging client in favor of general custom http client

--- client/client.go
+++ client/client.go
@@ -3,43 +3,42 @@ package client
 import (
 	"context"
 	"net/http"
-	"os"
 
-	"github.com/rs/zerolog"
 	apiClient "github.com/smartcontractkit/cvn-api-go/client"
 )
 
 // CVNClient is a client for the CVN API.
 type CVNClient = apiClient.ClientWithResponses
 
-// NewCVNClient creates a new CVN client with the specified base URL.
+// NewCVNClient creates a new CVN client with the specified base URL and API key.
 // It returns a pointer to the CVNClient and an error if any issues occur during initialization.
 //   - baseURL: The base URL of the CVN API.
+//   - apiKey: The API key for authenticating with the CVN API.
 func NewCVNClient(baseURL string, apiKey string) (*CVNClient, error) {
-	apiKeyHeaderEditor := func(ctx context.Context, req *http.Request) error {
-		req.Header.Set("Api-Key", apiKey)
-		return nil
-	}
-
-	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
-	customHttpClient := NewHTTPClientWithCURLLogger(&logger, nil)
-
-	return apiClient.NewClientWithResponses(baseURL,
-		apiClient.WithRequestEditorFn(apiKeyHeaderEditor),
-		apiClient.WithHTTPClient(customHttpClient),
-	)
+	return NewCVNClientWithHTTPClient(baseURL, apiKey, nil)
 }
 
-func NewCVNClientWithHTTPClient(baseURL, apiKey string, httpClient *http.Client, lgr *zerolog.Logger) (*CVNClient, error) {
+// NewCVNClientWithHTTPClient creates a new CVN client with the specified base URL and API key, using the provided HTTP client.
+// It returns a pointer to the CVNClient and an error if any issues occur during initialization.
+//   - baseURL: The base URL of the CVN API.
+//   - apiKey: The API key for authenticating with the CVN API.
+//   - httpClient: The custom HTTP client to use for making requests. If nil, the default HTTP client is used.
+func NewCVNClientWithHTTPClient(baseURL, apiKey string, httpClient *http.Client) (*CVNClient, error) {
 	apiKeyHeaderEditor := func(ctx context.Context, req *http.Request) error {
 		req.Header.Set("Api-Key", apiKey)
 		return nil
 	}
 
-	wrappedHTTPClient := NewHTTPClientWithCURLLogger(lgr, httpClient)
-
-	return apiClient.NewClientWithResponses(baseURL,
-		apiClient.WithRequestEditorFn(apiKeyHeaderEditor),
-		apiClient.WithHTTPClient(wrappedHTTPClient),
-	)
+	if httpClient == nil {
+		return apiClient.NewClientWithResponses(
+			baseURL,
+			apiClient.WithRequestEditorFn(apiKeyHeaderEditor),
+		)
+	} else {
+		return apiClient.NewClientWithResponses(
+			baseURL,
+			apiClient.WithRequestEditorFn(apiKeyHeaderEditor),
+			apiClient.WithHTTPClient(httpClient),
+		)
+	}
 }
