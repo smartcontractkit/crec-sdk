b18ef62991579012042f11397bdc24285c758cdd
Ilja Pavlovs <ilja.pavlovs@gmail.com>
2025-12-18 15:22:48 +0200

    BCM-1553: fix

--- events/events_test.go
+++ events/events_test.go
@@ -1150,7 +1150,7 @@ func TestClient_Verify(t *testing.T) {
 			Type:          apiClient.WatcherStatus,
 			WatcherId:     "550e8400-e29b-41d4-a716-446655440000",
 			ChainSelector: "5009297550715157269",
-			Status:        apiClient.WatcherStatusPayloadStatusDeploying,
+			Status:        apiClient.Deploying,
 			StatusCode:    "DEPLOYING",
 			StatusReason:  "Watcher is being deployed",
 		}
