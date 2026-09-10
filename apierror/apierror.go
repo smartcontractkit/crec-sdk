1c7b6230a1decb1f263350514ede19e53989b3b3
antonio moreno <thedownhiller@gmail.com>
2026-09-10 11:14:18 +0200

    Add ErrWalletNotReady conflict sentinel (#192)
    * Add ErrWalletNotReady conflict sentinel
    
    * Pin crec-api-go to tagged v0.9.0-rc2

--- apierror/apierror.go
+++ apierror/apierror.go
@@ -44,6 +44,7 @@ var (
 	ErrOperationNotCancellable  = errors.New("operation not cancellable")
 	ErrOperationDeadlineElapsed = errors.New("operation deadline elapsed")
 	ErrWalletAlreadyArchived    = errors.New("wallet already archived")
+	ErrWalletNotReady           = errors.New("wallet not ready to accept operations")
 	ErrChainUnavailable         = errors.New("chain unavailable for wallet creation")
 )
 
@@ -180,6 +181,8 @@ func Conflict(appErr *apiClient.ApplicationError) error {
 		return ErrOperationDeadlineElapsed
 	case apiClient.ApplicationErrorCodeWalletAlreadyArchived:
 		return ErrWalletAlreadyArchived
+	case apiClient.ApplicationErrorCodeWalletNotReady:
+		return ErrWalletNotReady
 	case apiClient.ApplicationErrorCodeChainUnavailable:
 		return ErrChainUnavailable
 	default:
